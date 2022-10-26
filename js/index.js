cc37973f50503c85b22cd417b10aa0f665caf852
oyenuga17 <oyenugaoluwaseun78@gmail.com>
2022-10-26 17:12:22 +0100

    modified edit button

--- js/index.js
+++ js/index.js
@@ -1,19 +1,51 @@
-function deleteTodo() {
- 
+
+let isEditMode = false;
+let editingIndex = null;
+
+const todoInputField = document.querySelector(".form__input")
+const todoListWrapper = document.querySelector(".todo__list")
+    const formButton = document.querySelector(".form__btn");
+function onEditClick(e) { 
+    formButton.innerText = "Edit"
+    const editButton = e.target;
+    const li = editButton.parentNode.parentNode
+    const todo__text = li.querySelector(".todo__text").innerText
+    todoInputField.value = todo__text
+
+    isEditMode = true;
+    editingIndex = li.id;
+    
 }
 
 
+function addListenersToEditButtons() {
+    const buttonCollection = document.querySelectorAll(".edit__btn");
+    const lastButton = buttonCollection[buttonCollection.length - 1];
+    lastButton.addEventListener("click",onEditClick);
+}
+
+function deleteTodo(e) {
+    const deleteButton = e.target;
+    
+    const li = deleteButton.parentNode.parentNode
+ 
+   li.remove();
+    
+}
 
 function addListenersToDeleteButtons() {
     const buttonCollection = document.querySelectorAll(".delete__btn");
     const lastButton = buttonCollection[buttonCollection.length - 1];
     
     lastButton.addEventListener("click", deleteTodo);
+    
 }
 
 
-function showTodoOutputValue(todo) {
+function addTodoToList(todo) {
     let li = document.createElement("li");
+
+    li.id = `todo-${todoListWrapper.getElementsByTagName('li').length}`;
 
     li.innerHTML = ` 
                     <div class="input__wrapper">
@@ -34,10 +66,20 @@ function showTodoOutputValue(todo) {
 
     li.classList.add("todo__list__section")
     todoListWrapper.appendChild(li);
-     addListenersToDeleteButtons();
+    addListenersToDeleteButtons();
+    addListenersToEditButtons();
+    todoInputField.value = ''
 
 }
 
+function editTodo(todo) {
+    const todoLi = document.getElementById(editingIndex);
+    todoLi.querySelector('.todo__text').innerText = todo;
+    isEditMode = false;
+    editingIndex = null;
+    todoInputField.value = ''
+    formButton.innerText = "Add"
+}
 
 
 function TodoFormSubmit(event) {
@@ -45,25 +87,18 @@ function TodoFormSubmit(event) {
 
     const todoInputValue = todoInputField.value;
 
-    showTodoOutputValue(todoInputValue);
+    if (isEditMode) {
+        editTodo(todoInputValue);
+    } else {
+        addTodoToList(todoInputValue);
+    }
 
 }
 
 
 function init() {
-    form = document.querySelector(".form")
-    editButton = document.querySelector(".edit__btn")
-    deleteBtn = document.querySelector(".delete__btn")
-    todoInputField = document.querySelector(".form__input")
-
-    todoListWrapper = document.querySelector(".todo__list")
-
-
-
+   const form = document.querySelector(".form")
     form.addEventListener("submit", TodoFormSubmit);
-
-    editButton.addEventListener("click", EditTodo);
-    deleteBtn.addEventListener("click", DeleteTodo);
 
 }
 
