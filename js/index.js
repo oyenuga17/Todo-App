7ffe9a7dc5a944707cf759b77cdfae0fc19dda16
oyenuga17 <oyenugaoluwaseun78@gmail.com>
2022-10-27 21:30:06 +0100

    validate input

--- js/index.js
+++ js/index.js
@@ -1,48 +1,49 @@
 
-let isEditMode = false;
+let MODE = "ADD";
 let editingIndex = null;
 
 const todoInputField = document.querySelector(".form__input")
 const todoListWrapper = document.querySelector(".todo__list")
-    const formButton = document.querySelector(".form__btn");
-function onEditClick(e) { 
+const formButton = document.querySelector(".form__btn");
+
+function onEditClick(e) {
     formButton.innerText = "Edit"
     const editButton = e.target;
     const li = editButton.parentNode.parentNode
     const todo__text = li.querySelector(".todo__text").innerText
     todoInputField.value = todo__text
-
-    isEditMode = true;
+    MODE= "EDIT";
     editingIndex = li.id;
-    
+
 }
 
 
 function addListenersToEditButtons() {
     const buttonCollection = document.querySelectorAll(".edit__btn");
     const lastButton = buttonCollection[buttonCollection.length - 1];
-    lastButton.addEventListener("click",onEditClick);
+    lastButton.addEventListener("click", onEditClick);
 }
 
 function deleteTodo(e) {
     const deleteButton = e.target;
-    
+
     const li = deleteButton.parentNode.parentNode
- 
-   li.remove();
-    
+
+    li.remove();
+
 }
 
 function addListenersToDeleteButtons() {
     const buttonCollection = document.querySelectorAll(".delete__btn");
     const lastButton = buttonCollection[buttonCollection.length - 1];
-    
+
     lastButton.addEventListener("click", deleteTodo);
-    
+
 }
 
 
 function addTodoToList(todo) {
+
     let li = document.createElement("li");
 
     li.id = `todo-${todoListWrapper.getElementsByTagName('li').length}`;
@@ -66,38 +67,50 @@ function addTodoToList(todo) {
 
     li.classList.add("todo__list__section")
     todoListWrapper.appendChild(li);
+
     addListenersToDeleteButtons();
     addListenersToEditButtons();
     todoInputField.value = ''
 
 }
 
+
 function editTodo(todo) {
     const todoLi = document.getElementById(editingIndex);
     todoLi.querySelector('.todo__text').innerText = todo;
-    isEditMode = false;
+    MODE = "ADD";
     editingIndex = null;
     todoInputField.value = ''
     formButton.innerText = "Add"
 }
 
+function validateForm(x) {
+  if (x == "" || x == null ) {
+    alert("add a todo");
+      return false 
+    }
+    return true
+}  
 
 function TodoFormSubmit(event) {
-    event.preventDefault();
+   event.preventDefault();
+  
+const todoInputValue = todoInputField.value;
 
-    const todoInputValue = todoInputField.value;
-
-    if (isEditMode) {
-        editTodo(todoInputValue);
-    } else {
-        addTodoToList(todoInputValue);
+if (validateForm(todoInputValue)) {
+        switch (MODE) {
+        case "EDIT":
+            editTodo(todoInputValue);
+            break;
+        default:
+            addTodoToList(todoInputValue);
     }
+    }  
 
-}
-
+  }
 
 function init() {
-   const form = document.querySelector(".form")
+    const form = document.querySelector(".form")
     form.addEventListener("submit", TodoFormSubmit);
 
 }
